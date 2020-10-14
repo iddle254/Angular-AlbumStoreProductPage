7b8c07302433a016ae540d1636e7422ed8c3aa2f
Charles <charlesiddle99@gmail.com>
2020-10-14 21:02:42 +0300

    deploying to firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMNJoB059JaqamnhT3gOUVvu8pW_zfcZ4",
+  authDomain: "albumstore-ad536.firebaseapp.com",
+  databaseURL: "https://albumstore-ad536.firebaseio.com",
+  projectId: "albumstore-ad536",
+  storageBucket: "albumstore-ad536.appspot.com",
+  messagingSenderId: "320936850776",
+  appId: "1:320936850776:web:68a9281228e3017563e12d"
+};
